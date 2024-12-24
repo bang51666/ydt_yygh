f4c003615254211230a598c468dc6a385351b92a
bang <1364782426@qq.com>
2024-12-24 23:30:16 +0800

    修改查看医院详情信息bug2

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalController.java
@@ -8,7 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
